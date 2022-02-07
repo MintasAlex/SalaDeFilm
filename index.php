dfca22afed5f761be4814cd9325fd5f4682cf079
Alex Mintas <alexmintas@gmail.com>
2022-02-07 21:28:08 +0200

    Added meta description, author, keywords

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 <html lang="en">
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="SalaDeFilm este un proiect studentesc care a are scopul de a testa functionalitati SEO">
+		<meta name="author" content="Mintas Alex">
+		<meta name="keywords" content="SalaDeFilm, Filme, Heroku">
 		<title>Program</title>
 		<link rel="icon" href="img/icon.png">
 		<link rel="stylesheet" href="css/style.css">
